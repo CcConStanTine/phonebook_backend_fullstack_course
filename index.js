7e15f139d3d10e57f781126b9fbe05e3c59eaff3
Paweł Karpiel <pawelkarpiel@Pawes-MacBook-Air.local>
2022-07-10 14:28:29 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+//app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let people = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/info', (req, res) => {
+  const dateNow = new Date()
+  const responseString = `
+    <div>
+      <p>Phonebook has info of ${people.length} people</p>
+      <p>${dateNow.toString()}
+    </div>
+  `
+  res.send(responseString)
+})
+
+app.get('/api/people', (req, res) => {
+  console.log("hi")
+  res.json(people)
+})
+
+app.get('/api/people/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = people.find((p) => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/people', (req, res) => {
+  const body = req.body
+
+  if ((!body.name) || (!body.number)) {
+    return res.status(400).json({
+      error: 'content or number missing in request body'
+    })
+  }
+  if (people
+    .map(person => person.name)
+    .find(name => name === body.name)
+  ) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: generateId(), 
+    name: body.name,
+    number: body.number
+  }
+
+  people = people.concat(person)
+
+  res.json(person)
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000000)
+}
+
+app.delete('/api/people/:id', (req, res) => {
+  const id = Number(req.params.id)
+  people = people.filter((p) => p.id !== id)
+  res.status(204).end()
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
